1c75f13bbd55f52a2ccd19c01b1cf356e889682f
s-sandhu2000 <49246075+s-sandhu2000@users.noreply.github.com>
2020-12-28 21:43:18 -0800

    navbar.js file started

--- _scripts/navbar.js
+++ _scripts/navbar.js
@@ -2,3 +2,18 @@
 /* navbar.js */
 
 
+
+console.log("Hello world!");
+const navbar_content = `
+<a>Home</a>
+<a href="Officers/">Officers</a>
+<a>Kenya/Panama Team</a>
+<a>Rwanda Team</a>
+<a>Local Team</a>
+<a>Industries Without Borders</a>
+<a>Fundraisers</a>
+<img id="Globe-Logo" src="_images/EWB-UCSB-Logo.jpg" height="100px">
+`;
+const NavBar = document.getElementById("NavBar");
+console.log(NavBar);
+NavBar.innerHTML = navbar_content;
