a1df99d25d1be1538db191134b75b4d6db13fcf4
E4tHam <43790149+E4tHam@users.noreply.github.com>
2020-12-28 22:02:50 -0800

    navbar.js finished

--- _scripts/navbar.js
+++ _scripts/navbar.js
@@ -2,18 +2,24 @@
 /* navbar.js */
 
 
+const dirlevel = parseInt(
+    document.currentScript.getAttribute( "dirlevel" )
+);
+const upADir = "../";
+const pathPrefix = upADir.repeat( dirlevel );
 
-console.log("Hello world!");
+// Fill NavBar Here
 const navbar_content = `
-<a>Home</a>
-<a href="Officers/">Officers</a>
+<a href="${pathPrefix}." >Home</a>
+<a href="${pathPrefix}Officers/">Officers</a>
 <a>Kenya/Panama Team</a>
 <a>Rwanda Team</a>
 <a>Local Team</a>
 <a>Industries Without Borders</a>
-<a>Fundraisers</a>
-<img id="Globe-Logo" src="_images/EWB-UCSB-Logo.jpg" height="100px">
+<a href="${pathPrefix}Fundraisers/">Fundraisers</a>
+<img id="Globe-Logo" src="${pathPrefix}_images/EWB-UCSB-Logo.jpg" height="100px">
 `;
+
 const NavBar = document.getElementById("NavBar");
-console.log(NavBar);
+
 NavBar.innerHTML = navbar_content;
